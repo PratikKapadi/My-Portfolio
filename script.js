baaae542f1c4a657688a0409a6a4460d8307f540
PratikKapadi <pratikkapdi13@gmail.com>
2026-02-14 20:32:43 +0530

    add exprince and certification and some skills

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let typed = new Typed("#auto-type", {
-  strings: ["MERN Stack Developer", "Web Developer", "Frontend Developer"],
+  strings: [
+    "App Devloper",
+    "React Native Devloper",
+    "MERN Stack Developer",
+    "Frontend Developer",
+  ],
   typeSpeed: 150,
   backSpeed: 150,
   loop: true,
+});
+
+// Scroll To Top Button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
